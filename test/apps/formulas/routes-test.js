2995a52a20a6d57c9628571642191fd47a3364da
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-03-22 16:06:25 -0300

    preparing for mocks/spies/stubs

--- test/apps/formulas/routes-test.js
+++ test/apps/formulas/routes-test.js
@@ -1,6 +1,9 @@
 describe('Formulas', function() {
   describe('GET /formulas', function() {
-    it('returns all formulas');
+    it('returns all formulas', function(done) {
+      var spy = sinon.spy;
+    });
+
     it('returns 200 okay');
     it('responds with json');
   });
